b2ff3c1b03e01fcf78ddd6a3b6a3ac7af11fb7b1
Ian DC Bautista <iandcbautista@Ians-MacBook-Pro.local>
2019-04-07 16:09:04 +0800

    commit having heroku

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-let WebSocketServer = require("ws").Server;
-let wss = new WebSocketServer ({ port: 8080 });
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
 
+app.use(express.static(__dirname + "/"))
 
-wss.on('connection', function connection(ws) {
-    
-    //ws.send('something');
-    
-    ws.on('open', function open() {
-        ws.send('something');
-    });
-      
-    ws.onmessage = (m) => {
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
+
+wss.on("connection", function(ws) {
+      //ws.send('something');
+      console.log("connection open");
+      setInterval(function() {
+              sendRnd10();      
+      }, 5000);
+      ws.on('open', function open() {
+          ws.send('something');
+      });
+
+      ws.on('message', function message(m) {
         console.log(m.data);
-    };
-    ws.onclose = () => {
+        client.send(m.data);
+      });
+        
+      ws.on('close', function close() {
         console.log("closed");
-    };
-
-    
-    //ws.close(); // <- this closes the connection from the server
-});
+      });
+  
+})
