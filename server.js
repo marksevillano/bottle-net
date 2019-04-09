f529e2a31d4fd669d40f6bd86d7b31a1b604026e
Ian DC Bautista <iandcbautista@Ians-MacBook-Pro.local>
2019-04-10 00:14:34 +0800

    Modified to add dispensinga nd confirmation events to send to nodemcu

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ console.log("http server listening on %d", port)
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
-
+//var client = null;
 wss.on("connection", function(ws) {
+   // client = ws;
       //ws.send('something');
       console.log("connection open");
      
@@ -27,16 +28,37 @@ wss.on("connection", function(ws) {
       ws.on('message', function message(m) {
         console.log(m);
         dataSent = m;
-        //ws.send(m.data);
+       // ws.send("recieved", m);
       });
-        
       ws.on('close', function close() {
         console.log("closed");
       });
-  
+      
 })
 
 app.get('/', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ bottle: dataSent }));
 })
+
+
+app.post('/confirm', function (req, res) {
+  wss.clients.forEach(function(client) {
+    //if(client.readyState === WebSocket.OPEN) {
+      client.send("CONFIRM");
+    //}
+  });
+
+  //client.send("CONFIRM");
+  res.end(JSON.stringify({ confirm: "success" }));
+});
+
+app.post('/dispense', function (req, res) {
+  wss.clients.forEach(function(client) {
+    //if(client.readyState === WebSocket.OPEN) {
+      client.send("DISPENSE");
+    //}
+  });
+  res.end(JSON.stringify({ confirm: "success" }));
+});
+
